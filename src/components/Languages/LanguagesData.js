b0ae9dae996e9dfef628f57d35b50313825cd011
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-12 13:06:50 -0800

    added languages data

---
+++ src/components/Languages/LanguagesData.js
@@ -0,0 +1,32 @@
+const LanguagesData = [
+    {
+        name: 'Java',
+        logo: ''
+    },
+    {
+        name: 'Javascript',
+        logo: ''
+    },
+    {
+        name: 'Python',
+        logo: ''
+    },
+    {
+        name: 'C',
+        logo: ''
+    },
+    {
+        name: 'Swift',
+        logo: ''
+    },
+    {
+        name: 'HTML/CSS',
+        logo: ''
+    },
+    {
+        name: 'React',
+        logo: ''
+    }
+]
+
+export default LanguagesData;
