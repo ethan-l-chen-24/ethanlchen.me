8dfac0c9560e8b36f85bf2d79d82769592370e70
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-12 12:25:00 -0800

    added education info

---
+++ src/components/Education/EducationData.js
@@ -0,0 +1,52 @@
+const EducationData = [
+    {
+        school: "Dartmouth College",
+        logo: "dartLogo",
+        location: "Hanover, NH",
+        years: "Sep 2020 - June 2024",
+        degree: "Pursuing a B.A. in Computer Science Modified w/ Engineering Sciences",
+        activities: [
+            {
+                name: "COSC 10, Problem Solving via Object-Oriented Programming",
+                position: "Teaching Assistant"
+            },
+            {
+                name: "Reality & Robotics Lab",
+                position: "Undergraduate Researcher"
+            },
+            {
+                name: "Club Squash Team",
+                position: "Member"
+            },
+            {
+                name: "Zeta Psi Fraternity",
+                position: "Brother"
+            }
+        ]
+    },
+    {
+        school: "Dartmouth College",
+        logo: "dartLogo",
+        location: "Hanover, NH",
+        years: "Sep 2020 - June 2024",
+        degree: "Robert Crowe Award Winner",
+        activities: [
+            {
+                name: "Cum Laude Society",
+                position: "Member"
+            },
+            {
+                name: "Peer Tutors",
+                position: "Head Tutor"
+            },
+            {
+                name: "Track and Field",
+                position: "Captain"
+            },
+            {
+                name: "Big Brothers Big Sisters of Western MA",
+                position: "Mentor"
+            }
+        ]
+    }
+]
