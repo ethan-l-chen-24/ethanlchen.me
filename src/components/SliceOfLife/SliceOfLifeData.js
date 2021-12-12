155f260c0f8d858e2e2a184a453a276a7e55ab8f
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-12 13:01:04 -0800

    added sliceOfLife data

---
+++ src/components/SliceOfLife/SliceOfLifeData.js
@@ -0,0 +1,25 @@
+const SliceOfLifeData = [
+    {
+        type: 'Piano',
+        description: 'Avid classical, jazz, and ani-pianist, and I also love doing arrangements!',
+        images: {
+
+        }
+    },
+    {
+        type: 'Skiing',
+        description: 'A former alpine racer, I now love hitting the slopes recreationally for the best terrain around!',
+        images: {
+
+        }
+    },
+    {
+        type: 'Roller Coasters',
+        description: 'I am a true enthusiast that gets more excited about roller coasters than anything else in the world (my favorites are X2 at Six Flags Magic Mountain and El Toro at Six Flags Great Adventure)!',
+        images: {
+
+        }
+    }
+]
+
+export default SliceOfLifeData;
