08ba40ec402607adde326d6cdfe82803eb9f3591
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-12 13:37:58 -0800

    added navbar data

---
+++ src/components/Navbar/NavbarData.js
@@ -0,0 +1,22 @@
+const NavbarData = [
+    {
+        section: 'About Me',
+        destination: ''
+    },
+    {
+        section: 'Education',
+        destination: ''
+    },
+    {
+        section: 'Languages',
+        destination: ''
+    },
+    {
+        section: 'Projects',
+        destination: ''
+    },
+    {
+        section: 'Contact Me',
+        destination: ''
+    }
+]
