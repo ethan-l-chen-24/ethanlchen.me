3274022860a0f66419a287641f5dd9c07043570e
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-12 12:54:19 -0800

    added projects data

---
+++ src/components/Projects/ProjectsData.js
@@ -0,0 +1,49 @@
+const ProjectsData = [
+    {
+        name: 'ethanlchen.me',
+        languages: [
+            'React',
+            'HTML',
+            'CSS',
+            'Javascript'
+        ],
+        date: 'Dec 2021',
+        github: 'https://github.com/ethan-l-chen-24/ethanlchen.me',
+        description: 
+            'A personal website that showcases my skills in UI/UX design'
+    },
+    {
+        name: 'Sudoku Solver',
+        languages: [
+            'C',
+            'Bash'
+        ],
+        date: 'Nov 2021',
+        github: 'https://github.com/ethan-l-chen-24/Sudoku-Solver',
+        description: 
+            'A final group project for CS50: Software Design and Implementation, this project creates and solves a 4x4, 9x9, or 16x16 sudoku board. It also contains server-client capabilities to communicate across machines.'
+    },
+    {
+        name: 'Pandemic',
+        languages: [
+            'Java',
+            'JavaFX'
+        ],
+        date: 'May 2020',
+        github: 'https://github.com/ethan-l-chen-24/Pandemic',
+        description: 
+            'A computer version of the popular board game Pandemic by ZMan games'
+    },
+    {
+        name: 'Simon Game',
+        languages: [
+            'Verilog'
+        ],
+        date: 'May 2019',
+        description:
+            'A physical replication of the Simon Game, connected to a programmed FPGA'
+    }
+
+]
+
+export default ProjectsData;
