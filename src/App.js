4555d530b9474eb1b008b380963ce61c6c602cfe
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-13 14:52:46 -0800

    implemented data into about me

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import AboutMe from './components/AboutMe'
-import AboutMe from './components/AboutMe'
-import AboutMe from './components/AboutMe'
+import AboutMe from './components/AboutMe/AboutMe'
+import Education from './components/Education/Education'
+import Languages from './components/Languages/Languages'
+import Projects from './components/Projects/Projects'
+import SliceOfLife from './components/SliceOfLife/SliceOfLife'
+import Contact from './components/Contact/Contact.js'
 
 function App() {
   return (
--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import AboutMeData from './AboutMeData'
 
 const AboutMe = () => {
     return (
         <div>
+            <div className='title'>{AboutMeData.name}</div>
             About Me
+            <p>{AboutMeData.description}</p>
         </div>
     )
 }
