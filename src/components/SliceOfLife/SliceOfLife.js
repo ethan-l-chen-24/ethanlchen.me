d74b02a3e27a35b1f405d312b7d9bba71eb7957f
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-14 21:50:11 -0800

    added shadow to icons

--- src/components/SliceOfLife/SliceOfLife.js
+++ src/components/SliceOfLife/SliceOfLife.js
@@ -4,17 +4,18 @@ import SliceOfLifeTile from './SliceOfLifeTile'
 
 const SliceOfLife = () => {
     return (
-        <div id="sliceOfLife">
-            <h1>Slice Of Life</h1>
-            <p className='quote'>
-                <i>“Look at us, riding around in a limo, eating hot dogs…it’s like we’re the president.”</i>
-                <div>- Marshall Erickson</div>
-            </p>
-            <hr id="sliceOfLifeHR"/>
-            {SliceOfLifeData.map((activity) => 
-            (<SliceOfLifeTile type={activity.type} description={activity.description} images={activity.images}/>)
-            )}
-        </div>
+            <div id="sliceOfLife">
+                <div className="fall half-opacity"></div>
+                <h1>Slice Of Life</h1>
+                <p className='quote'>
+                    <i>“Look at us, riding around in a limo, eating hot dogs…it’s like we’re the president.”</i>
+                    <div>- Marshall Erickson</div>
+                </p>
+                <hr id="sliceOfLifeHR"/>
+                {SliceOfLifeData.map((activity) => 
+                (<SliceOfLifeTile type={activity.type} description={activity.description} images={activity.images}/>)
+                )}
+            </div>
     )
 }
 
