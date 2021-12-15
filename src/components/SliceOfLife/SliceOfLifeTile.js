502e5408bd6fbd91b924e376ae652608b5aec825
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-14 18:34:07 -0800

    fixed positioning of text box

--- src/components/SliceOfLife/SliceOfLifeTile.js
+++ src/components/SliceOfLife/SliceOfLifeTile.js
@@ -5,10 +5,10 @@ const SliceOfLifeTile = ( { type, description, images } ) => {
     return (
         <Container>
             <Row className="activity">
-                <Col xs={12} md={3} className='center'>
+                <Col xs={12} md={3} className='center left-align'>
                     <u><h2>{ type }</h2></u>
                 </Col>
-                <Col xs={12} md={9} className='center'>
+                <Col xs={12} md={9} className='center left-align'>
                     <p className="boxed">{ description }</p>
                 </Col>
             </Row>
