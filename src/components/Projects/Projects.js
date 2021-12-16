0b30d1d1afa6d0f08ab1bfc72dab120da002808b
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-15 16:48:09 -0800

    started projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import ProjectsData from './ProjectsData'
 import ProjectTile from './ProjectTile'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Projects = () => {
     return (
-        <div>
-            <h1>Projects</h1>   
-            {ProjectsData.map((project) =>
-            (<ProjectTile name={project.name} languages={project.languages} date={project.date} github={project.github} description={project.description}/>)
-            )}
-        </div>
+        <section id="projects">
+            <h1>Projects</h1>  
+            <p className='quote'>
+                <i>“It’s funny sometimes you walk into a place and you know you’re exactly where you’re supposed to be.”</i>
+                <div>- Ted Mosby</div>
+            </p>
+            <hr id="projectsHR"/>
+             <Container>
+                <Row>
+                    {ProjectsData.map((project) =>
+                    (<Col xs={6} lg={3} className='center'>
+                        <ProjectTile name={project.name} languages={project.languages} date={project.date} github={project.github} description={project.description}/>
+                    </Col>)
+                    )}
+                </Row>      
+            </Container> 
+        </section>
     )
 }
 
