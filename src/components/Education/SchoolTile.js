1b33fea72d9d4c8cc841cbed192721b6d46588f6
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-16 15:51:01 -0800

    formatting text on right of education

--- src/components/Education/SchoolTile.js
+++ src/components/Education/SchoolTile.js
@@ -1,16 +1,31 @@
 import React from 'react'
-import { Col } from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
 
 const SchoolTile = ( { school, logo, location, years, degree, activities } ) => {
     return (
         <Col xs={12} lg={6}>
             <div className='boxed schoolBox'>
-                <div id={logo} className="schoolLogo">{ school }</div>
-                <div> { logo }</div>
-                <div> { location } </div>
-                <div> { years } </div>
-                <div> { degree } </div>
-                <div> { activities} </div>
+                <Row>
+                    <Col xs={7} sm={6} md={5}>
+                        <div id={logo} className="schoolLogo"></div>
+                    </Col>
+                    <Col xs={5} sm={6} md={7}>
+                        <Row>
+                            <Col xs={6}>
+                            
+                            </Col>
+                            <Col xs={6}>
+                            
+                            </Col>
+                        </Row>
+                        <div> { school }</div>
+                        <div class="vl"></div>
+                        <div> { location } </div>
+                        <div> { years } </div>
+                        <div> { degree } </div>
+                        <div> { activities} </div>
+                    </Col>
+                </Row>
             </div>
         </Col>
     )
