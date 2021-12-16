9d018ab8448717e7dc2a4646b88bc3fc87355c2d
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-15 17:37:34 -0800

    working on formatting of project tile

--- src/components/Projects/ProjectTile.js
+++ src/components/Projects/ProjectTile.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import { Row, Col } from 'react-bootstrap'
 
 const ProjectTile = ( { name, languages, date, github, description} ) => {
-    return (
-        <div className='boxed projectBox'>
-            <div>{ name + " | " + date }</div>
-            <hr className='projectHR'/>
+
+    const getProjectDescription = () => {
+       return (
+        <div>
+        <div>{ name + " | " + date }</div>
+            <hr className='projectTileHR'/>
             <p>{ description }</p>
-            <div> {languages.map((language, i, arr) => (i === arr.length-1 ? (<span> {language} </span>) : (<span>{language + ', '}</span>)))} </div>
+            <Row>
+                <Col xs={8}>
+                    <div> <u>Tags</u> </div>
+                    <p>{languages.map((language, i, arr) => (i === arr.length-1 ? (<span> {language} </span>) : (<span>{language + ', '}</span>)))}</p>
+                </Col>
+                <Col xs={4}>
+                
+                </Col>
+            </Row>
+
+        </div>
+       )
+    }
+
+    return (
+        <div className='boxed projectBox center'>
+           {getProjectDescription()}
         </div> 
     )
 }
