6009199e0a4fe7cad3887a1eacc3bbe4d7b96272
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2021-12-15 21:37:56 -0800

    working on star ratings for each skill

--- src/components/Abilities/Abilities.js
+++ src/components/Abilities/Abilities.js
@@ -6,6 +6,11 @@ const Languages = () => {
     return (
         <section id="abilities">
             <h1>Abilities</h1>
+            <p className='quote'>
+                <i>“Oprah wasn't built in a day.”</i>
+                <div>- Lily Aldrin</div>
+            </p>
+            <hr id="abilitiesHR"/>
             { AbilitiesData.map((language) =>
                 (<AbilitiesSection sectionName={language.sectionName} skills={language.skills} />)
             )}
--- src/components/Abilities/AbilityTile.js
+++ src/components/Abilities/AbilityTile.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import { Col } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 
 const AbilityTile = ( { skill } ) => {
+
+    const alt = 5 - skill.score;
+
     return (
-        <Col>
-            <div> {skill.name} </div>
-            <div> {skill.score} </div>
-            <div>hello</div> 
+        <Col xs={12} md={6} lg={4}>
+            <Row>
+                <Col xs={8} className='skillCol'>
+                    {skill.name}
+                </Col>
+                <Col xs={4}className='scoreCol'>
+                    {[...Array(alt)].map(() => 
+                        (<span className="emptyCircle"></span>)
+                    )}
+                    {[...Array(skill.score)].map(() => 
+                        (<span className="fullCircle"></span>)
+                    )}
+                </Col>
+            </Row>
         </Col>
     )
 }
