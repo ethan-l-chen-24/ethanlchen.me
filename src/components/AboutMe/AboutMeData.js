81bcc3f21e779e9f84b636422c00f8ed7c907d2f
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2022-10-20 03:22:09 +0800

    add change back

--- src/components/AboutMe/AboutMeData.js
+++ src/components/AboutMe/AboutMeData.js
@@ -1,6 +1,6 @@
 const AboutMeData = {
     name: 'Ethan Chen',
-    description: `Hello World! Originally from Carson City, NV, I am a member of Dartmouth College's class of 2024 where I intend to pursue Computer Science and Engineering. I enjoy exploring new frameworks and languages, as I always strive to improve my professional coding abilities. I hope to employ my industry knowledge in Full-Stack development while also learning about AI or VR/AR!`
+    description: `Hello World! Originally from Carson City, NV, I am a member of Dartmouth College's class of 2024 where I intend to pursue Computer Science and Engineering. I enjoy exploring new frameworks and languages, as I always strive to improve my professional coding abilities. I hope to employ my industry knowledge in Full-Stack development while also learning about AI or VR/AR! I am excited to announce that I have accepted an offer as a Software Engineering Intern at Roblox for Summer 2023!`
 }
 
 export default AboutMeData
