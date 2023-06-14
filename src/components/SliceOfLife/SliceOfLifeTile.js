c4e84ed4a4399645b2c55932921d986bbd30673a
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2023-06-14 16:03:32 -0600

    underline removed

--- src/components/SliceOfLife/SliceOfLifeTile.js
+++ src/components/SliceOfLife/SliceOfLifeTile.js
@@ -19,7 +19,7 @@ const SliceOfLifeTile = ( { type, description, images } ) => {
         <Container>
             <Row className="activity">
                 <Col xs={12} md={3} className='center left-align'>
-                    <h2><u>{ type }</u></h2>
+                    <h2>{ type }</h2>
                 </Col>
                 <Col xs={12} md={9} className='center'>
                     <p className="boxed solDescription">{ description }</p>
